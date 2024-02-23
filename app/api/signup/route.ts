615a80304f4f8e1c59d550e8f186368d1ed1e940
Yusuf Shaikh <yusufshaikh94146@gmail.com>
2024-02-23 11:18:54 +0530

    signup endpoint

---
+++ app/api/signup/route.ts
@@ -0,0 +1,25 @@
+import { sql } from "@/db";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(request:Request){
+    const json = await request.json();
+
+    const res = await sql(
+        "select id, username from users where username ilike $1",
+        [json.username]
+    );
+
+    if(res.rowCount > 0){
+        return NextResponse.json({error: "User already exists"})
+    }
+
+    const saltRounds = 10;
+    const hash = await bcrypt.hash(json.password,saltRounds);
+
+    await sql("insert into users (username, password) values ($1,$2)",
+    [json.username,hash]
+    )
+
+    return NextResponse.json({msg: "Registration successful"}, {status: 201});
+}
