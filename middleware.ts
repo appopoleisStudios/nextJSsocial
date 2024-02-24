4be2f1672946ae4aebf10a1ab5ea15ed5c89ae9d
Yusuf Shaikh <yusufshaikh94146@gmail.com>
2024-02-24 12:32:02 +0530

    auth authenticated

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { jwtVerify } from "jose";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(request: NextRequest){
+    const pathname = request.nextUrl.pathname;
+
+    const authenticatedAPIRoutes = [
+        pathname.startsWith("/api/users")
+    ]
+
+    if (authenticatedAPIRoutes.includes(true)){
+        const cookie = request.cookies.get("jwt-token")
+
+        if(!cookie || !cookie?.value){
+            return NextResponse.json({error: "UnAuthenticated"},{status:401})
+        }
+
+        try {
+            const secret = new TextEncoder().encode(process.env.JWT_SECRET!)
+            await jwtVerify(cookie.value, secret)
+        } catch (error) {
+            console.error(error)
+            return NextResponse.json({error:"Internal Server Error"},{status:500})
+        }
+    }
+}
+
+export const config = {
+    matchar: "/:path*",
+}
