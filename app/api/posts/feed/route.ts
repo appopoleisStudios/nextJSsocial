692edcc667f21bc4200973b97bf2bcad10771389
Yusuf Shaikh <yusufshaikh94146@gmail.com>
2024-02-26 09:52:53 +0530

    fix 500 error

--- app/api/posts/feed/route.ts
+++ app/api/posts/feed/route.ts
@@ -12,7 +12,7 @@ export async function GET(request:Request){
     const res = await sql(`select p.*, u.username, u.avatar from posts p 
         inner join users u on p.user_id = u.id  where user_id in 
         (select user_id from follows where follower_id = $1) 
-        order by created_at desc  limit $2 offset $3;`,[jwtPayload.sub, limit, offset]);
+        order by created_at desc limit $2 offset $3;`,[jwtPayload.sub, limit, offset]);
 
     return NextResponse.json({data:res.rows});
 }
